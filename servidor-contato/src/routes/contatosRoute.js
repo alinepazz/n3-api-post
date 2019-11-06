19a01e406e87cfd432230ee15d16db3ef2d3858f
alinepazz <aline.soares.2@hotmail.com>
2019-11-06 18:15:21 -0300

    alterações e inclusão do POST

--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
+var bodyParser = require("body-parser")
 const controller = require("../controller/contatosController")
 
 router.get("/", controller.getAll)
 router.get("/:id", controller.getById)
+router.post("criar", bodyParser.json(), controller.add)
 
 module.exports = router
