11ec4d490fec6d0441651d3b5b81fea515cfd71a
Aline <aline.soares.2@hotmail.com>
2019-11-08 21:52:44 -0300

    Colocando condição de não aceitar dados incorretos

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -5,34 +5,37 @@ const getAll = (request, response) => {
   response.status(200).send(model.agenda);
 };
 
-// 
+// Salvar apenas contatos diferentes. Não queremos salvar contatos repetidos na nossa base de dados;
 
 const add = (request, response) => {
   let contato = request.body
   let baseDados = model.agenda.contatos
 
+
   contato.id = Math.random().toString(36).substr(-8)
-// Salvar apenas contatos diferentes. Não queremos salvar contatos repetidos na nossa base de dados;
 
-  if (baseDados.find(dado => dado.nome === contato.nome)){
-    response.status(400).send("Contato já cadastrado")
-} else {
-  model.agenda.contatos.push(contato)
-  response.status(201).send(contato) //serve para colocar algo dentro do contatos.js
- };
+  if (!contato.nome || !contato.dataNascimento || contato.celular) {
+    response.status(400).send("Informações incorretas")
+  }
 
-  
-   
+  else {
+    if (baseDados.find(dado => dado.nome === contato.nome)) {
+      response.status(400).send("Contato já cadastrado")
+    }
+    else {
+      model.agenda.contatos.push(contato)
+      response.status(201).send(contato) //serve para colocar algo dentro do contatos.js
+    }
+  }
+
 }
 
 
-
-
-module.exports = {
-  getAll,
- // getById
-  add
-}
+  module.exports = {
+    getAll,
+    // getById
+    add
+  }
 
 
 
